7d48f1896123d2e563c56ba7bded52f549123b60
dhanush997 <46255447+dhanush997@users.noreply.github.com>
2021-05-07 17:09:49 +0530

    upload

---
+++ Datatypes.java
@@ -0,0 +1,37 @@
+package DataTypes;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		
+		short a=12;
+		
+		int b=90;
+		
+		long c=1313131313131313131L;
+		
+		float d=12.2424131f;
+		
+		double e=131313.13132213256d;
+		
+		char f='$';
+		
+		String g="Hello my name is Dhanush";
+		
+		boolean h=true;
+		
+		boolean i=false;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(i);
+
+	}
+
+}
